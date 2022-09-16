50c20ded8f5a3c9912e3a0813022554d3d107b42
Andy Kuan <andy952737@gmail.com>
2022-09-16 20:45:21 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
+import AndyKuanDev from "./pages/AndyKuanDev";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
@@ -11,13 +12,14 @@ import styles from "./styles.css";
 
 // default
 export function App() {
-  return (
+  return ( 
     <BrowserRouter>  
-      <Routes> 
+      <Routes>  
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="gitbooks" element={<GitBooks />} />
+          <Route path="andykuan_dev" element={<AndyKuanDev />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
---
+++ src/pages/AndyKuanDev.js
@@ -0,0 +1,10 @@
+// 為了gihub.io使用的特殊首頁 
+const Home = () => {
+  return (
+    <>
+      <p className="home_text">Hi, 我是一個網站後端工程師, 期望不斷精進在開發程式道路上，創造更好的產品，能被世界所使用。</p>
+    </>
+  )
+};
+
+export default Home; 
