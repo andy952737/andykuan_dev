0afa452d08f3626e7c11d19816c6165b5959469d
Andy Kuan <andy952737@gmail.com>
2022-09-16 19:55:48 +0800

    restart

---
+++ src/pages/Layout.js
@@ -0,0 +1,29 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <nav className="nav_a">
+        <ul>
+          <li>
+            <Link to="/">AndyKuan Dev</Link>
+            <p className="title_discrip">黑暗中<span className="title_sub_discrip">紅寶石</span></p>
+          </li>
+          <li>
+            <a href="https://medium.com/@lianankuan" target="_blank">Tech Blogs</a>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li> 
+        </ul>
+      </nav>
+
+      <Outlet />
+
+      {/*<footer className="footer_b"> Footer </footer>*/}
+
+    </>
+  )
+};
+
+export default Layout;
