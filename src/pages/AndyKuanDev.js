1b5920a629a26654d9360380cd64234b38c52578
Andy Kuan <andy952737@gmail.com>
2022-09-17 10:01:21 +0800

    update avatar

--- src/pages/AndyKuanDev.js
+++ src/pages/AndyKuanDev.js
@@ -1,11 +1,19 @@
 // 為了gihub.io使用的特殊首頁 
-const Home = () => {
-  return (
-    <>
-      <p className="home_text">Hi, 我是一個網站後端工程師, <br />期望不斷精進在開發程式道路上，<br />創造更好的產品，能被世界所使用。</p>
-      <hr />
-    </>
-  )
-};
+import React, { Component } from 'react';
+import avatar from "../images/andy.jpg";
 
-export default Home; 
+class Home extends Component {
+  
+  render() {
+    return (
+      <div className="home_main">
+          <img className="my_avatar" src={avatar} />
+          <p className="home_text">Hi, 我是一個網站後端工程師, <br />期望不斷精進在開發程式道路上，<br />創造更好的產品，能被世界所使用。</p>
+          <hr /> 
+      </div>
+    );
+  } 
+
+}
+
+export default Home;
