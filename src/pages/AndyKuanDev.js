e8250689af76f774a50aefd56b5aa722813186f1
Andy Kuan <andy952737@gmail.com>
2022-10-06 19:27:47 +0800

    update home page

--- src/pages/AndyKuanDev.js
+++ src/pages/AndyKuanDev.js
@@ -1,4 +1,3 @@
-// 為了gihub.io使用的特殊首頁 
 import React, { Component } from 'react';
 import avatar from "../images/andy.jpg";
 
@@ -6,11 +5,26 @@ class Home extends Component {
   
   render() {
     return (
+      <>
       <div className="home_main">
           <img className="my_avatar" src={avatar} />
           <p className="home_text">Hi, 我是一個網站後端工程師, <br />期望不斷精進在開發程式道路上，<br />創造更好的產品，能被世界所使用。</p>
           <hr /> 
       </div>
+
+      <div className="home_main_contact">
+        <h3>社群&興趣</h3>
+        程式論壇, 生活街舞, 攝影日常
+        <hr /> 
+      </div>
+
+      <div className="home_main_contact">
+        <h3>聯絡信箱</h3>
+        andykuan@protonmail.com 
+        <hr /> 
+      </div>
+
+      </>
     );
   } 
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,19 @@ class Home extends Component {
   
   render() {
     return (
+      <>
       <div className="home_main">
           <img className="my_avatar" src={avatar} />
           <p className="home_text">Hi, 我是一個網站後端工程師, <br />期望不斷精進在開發程式道路上，<br />創造更好的產品，能被世界所使用。</p>
           <hr /> 
       </div>
+
+      <div className="home_main_contact">
+        <h3>社群&興趣</h3>
+        程式論壇, 生活街舞, 攝影日常
+      </div>
+
+      </>
     );
   } 
 
