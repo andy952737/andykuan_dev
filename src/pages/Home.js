8de0bdf287a6cbd575e7cd4970929bdb7abec615
Andy Kuan <andy952737@gmail.com>
2022-10-17 14:11:18 +0800

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
       <div className="home_main">
           <img className="my_avatar" src={avatar} />
           <p className="home_text">Hi, 我是一個網站全端工程師, <br />期望不斷精進在開發程式道路上，<br />創造更好的產品，能被世界所使用。</p>
-          <hr /> 
+          <hr />  
       </div>
 
       <div className="home_main_contact"> 
